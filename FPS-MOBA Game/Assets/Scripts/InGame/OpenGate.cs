52ca71141418fbd8fe0eadd8f26282aa2722f768
Daniel Munch <daniellmunch@gmail.com>
2018-06-05 21:13:30 +0200

    Updated the countdown thingy

--- FPS-MOBA Game/Assets/Scripts/InGame/OpenGate.cs
+++ FPS-MOBA Game/Assets/Scripts/InGame/OpenGate.cs
@@ -6,16 +6,24 @@ using UnityEngine.UI;
 
 public class OpenGate : MonoBehaviour {
 
+    // Getting the gates
     public Transform gate1;
     public Transform gate2;
 
+    // Getting the countdown text
     public Text countdownText;
+    // Making a float called curCountdownNum. Whick will be storing the countdown
     private float curCountdownNum;
 
+    // Getting the 2 colors
+    public Color yellow;
+    public Color red;
+
+    // Start is called in the beginning
     void Start()
     {
         // Sets the length of the countdown
-        curCountdownNum = 5f;
+        curCountdownNum = 20f;
 
         // Disables the countdown text
         countdownText.enabled = false;
@@ -29,7 +37,7 @@ public class OpenGate : MonoBehaviour {
         // Checks if theres enough players in the match. (Change the amount later!)
         if (nm.numPlayers == 1)
         {
-            if (curCountdownNum <= 5 && curCountdownNum > 0)
+            if (curCountdownNum <= 20 && curCountdownNum > 0)
             {
                 // Enables the countdown text
                 countdownText.enabled = true;
@@ -39,6 +47,20 @@ public class OpenGate : MonoBehaviour {
 
                 // Makes the countdown text the same as the countdown number
                 countdownText.text = curCountdownNum.ToString("F0");
+
+                // Checks if the countdown text is within 10 to 5
+                if (curCountdownNum <= 10 && curCountdownNum >= 6)
+                {
+                    // Sets the color of the countdown text to yellow
+                    countdownText.color = yellow;
+                }
+
+                // Checks if the countdown text is within 5 to 0
+                if (curCountdownNum <= 5 && curCountdownNum >= 0)
+                {
+                    // Sets the color of the countdown text to red
+                    countdownText.color = red;
+                }
 
                 // Checks if the countdown has reached 0
                 if (curCountdownNum == 0f || countdownText.text == "0")
