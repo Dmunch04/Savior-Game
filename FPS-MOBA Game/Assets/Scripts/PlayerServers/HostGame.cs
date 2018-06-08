448e036773cf64dc564715506fd966976777afcc
Daniel Munch <daniellmunch@gmail.com>
2018-06-08 17:07:35 +0200

    Started working on legend selection

--- FPS-MOBA Game/Assets/Scripts/PlayerServers/HostGame.cs
+++ FPS-MOBA Game/Assets/Scripts/PlayerServers/HostGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HostGame : MonoBehaviour {
 
