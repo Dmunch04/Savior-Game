0825ea7c3dfb86bf3107518bd68a4b952107f480
Daniel Munch <daniellmunch@gmail.com>
2018-06-08 17:38:52 +0200

    Worked more on selection menu

---
+++ FPS-MOBA Game/Assets/HostManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HostManager : MonoBehaviour {
+
+    public Text errorText;
+
+    [HideInInspector]
+    public string matchName;
+
+    public void SetMatchName(string _name)
+    {
+        matchName = _name;
+    }
+
+    public void StartMatch ()
+    {
+        if (matchName != "" && matchName != null)
+        {
+            SceneManager.LoadScene("LegendSelection");
+        }
+        else
+        {
+            errorText.text = "Please give the match a name!";
+        }
+    }
+
+}
--- FPS-MOBA Game/Assets/Scripts/PlayerServers/HostGame.cs
+++ FPS-MOBA Game/Assets/Scripts/PlayerServers/HostGame.cs
@@ -13,11 +13,17 @@ public class HostGame : MonoBehaviour {
     private uint matchSize = 10;
 
     private string matchName;
+    private string _matchName;
 
     private NetworkManager networkManager;
+    private HostManager hm;
 
     void Start()
     {
+        hm = GetComponent<HostManager>();
+
+        _matchName = hm.matchName;
+
         networkManager = NetworkManager.singleton;
 
         if (networkManager.matchMaker == null)
@@ -29,6 +35,13 @@ public class HostGame : MonoBehaviour {
     public void SetMatchName (string _name)
     {
         matchName = _name;
+    }
+
+    public void MakeMatch ()
+    {
+        Debug.Log("Creating Match: " + _matchName + " with room for " + matchSize + " players.");
+
+        networkManager.matchMaker.CreateMatch(_matchName, matchSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
     }
 
     public void CreateMatch ()
